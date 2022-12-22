f3a6cb6143c0c0102e29c05b5fcc56cc2eaf3254
wilson Neto <wngaspar10@gmail.com>
2022-12-22 17:19:48 -0300

    test: should add user with complete data to mainling list

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,14 @@
+import { UserData } from "./user-data"
+
+describe('Register user on mailing list use case', () => {
+    test('should add user with complete data to mailing list', async () => {
+        const users: UserData[] = []    
+        // const repo: UserRepository = new inMemoryUserRepository(users)
+        // const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+        // const name = 'any_name'
+        // const email = 'any@email.com'
+        // const response = await usecase.registerUserOnMailingList({ name, email })
+        // const user = repo.findUserByEmail('any@email.com')
+        // expect((await user).name).toBe('any_name')
+    })
+})
