0f6674aa752e04f90f8d8e11d2aed450a398abe8
wilson Neto <wngaspar10@gmail.com>
2022-12-22 18:45:00 -0300

    test: add and find user and checks its existence and find all users

--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -1,10 +1,21 @@
+import { UserData } from "../user-data"
 import { InMemoryUserRepository } from "./in-memory-user-repository"
 
 describe('In memory User repository', () => {
   test('should return null if user is not found', async () => {
-    const users = []
+    const users: UserData[] = []
     const userRepo = new InMemoryUserRepository(users)
     const user = await userRepo.findUserByEmail('any@mail.com')
     expect(user).toBeNull()
+  })
+
+  test('should return user if it is found in the repository', async () => {
+    const users: UserData[] = []
+    const name = 'any_name'
+    const email = 'any@mail.com'
+    const userRepo = new InMemoryUserRepository(users)
+    await userRepo.add({ name, email })
+    const user = await userRepo.findUserByEmail('any@mail.com')
+    expect(user.name).toBe('any_name')
   })
 })
