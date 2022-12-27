238cb65fa52b0e3aa08afcc96f47fbd6e18a2943
wilson Neto <wngaspar10@gmail.com>
2022-12-27 18:34:51 -0300

    feat: validate emails with domain parts larger than 63 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -18,6 +18,14 @@ export class Email {
       return false
     }
 
+    const domainParts = domain.split('.')
+
+    if (domainParts.some(function (part) {
+      return part.length > 63
+    })) {
+      return false
+    }
+
     return true
   }
 }
