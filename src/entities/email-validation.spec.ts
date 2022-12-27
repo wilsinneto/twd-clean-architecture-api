1746bb45bc7a82a9bf397c3f6408457388222f06
wilson Neto <wngaspar10@gmail.com>
2022-12-27 15:03:40 -0300

    test: validate emails with empty local part

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -31,4 +31,9 @@ describe('Email validation', () => {
     const email: string = 'local@' + 'd'.repeat(128) + '.' + 'd'.repeat(127)
     expect(Email.validate(email)).toBeFalsy()
   })
+
+  test('should not accept empty local part', () => {
+    const email = '@mail.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })
