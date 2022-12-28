0e2931082dab7da3ddc48872a43ece0e92b275fe
wilson Neto <wngaspar10@gmail.com>
2022-12-28 13:49:12 -0300

    feat: create valid user

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,4 +1,4 @@
-import { Either, left } from '../shared/either';
+import { Either, left, right } from '../shared/either';
 import { Email } from './email';
 import { InvalidEmailError } from './errors/invalid-email-error';
 import { InvalidNameError } from './errors/invalid-name-error';
@@ -6,6 +6,14 @@ import { Name } from './name';
 import { UserData } from './user-data';
 
 export class User {
+  public readonly email: Email
+  public readonly name: Name
+
+  private constructor (name: Name, email: Email) {
+    this.name = name
+    this.email = email
+  }
+
   static create(userData: UserData): Either<InvalidNameError | InvalidEmailError, User> {
     const nameOrError = Name.create(userData.name)
     if (nameOrError.isLeft()) {
@@ -16,5 +24,10 @@ export class User {
     if (emailOrError.isLeft()) {
       return left(new InvalidEmailError())
     }
+
+    const name: Name = nameOrError.value as Name
+    const email: Email = emailOrError.value as Email
+
+    return right(new User(name, email))
   }
 }
