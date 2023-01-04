23989d488fdcd1b27082e41908f19669c75b0345
wilson Neto <wngaspar10@gmail.com>
2023-01-04 18:35:11 -0300

    test: web controller should return 500 when server raises

--- test/web-controllers/register-user-controller.spec.ts
+++ test/web-controllers/register-user-controller.spec.ts
@@ -1,5 +1,6 @@
 import { UserData } from '@/entities'
 import { InvalidEmailError, InvalidNameError } from '@/entities/errors'
+import { UseCase } from '@/usecases/ports'
 import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
 import { UserRepository } from '@/usecases/register-user-on-mailing-list/ports'
 import { MissingParamError } from '@/web-controllers/errors/missing-param-error'
@@ -10,8 +11,16 @@ import { InMemoryUserRepository } from '@test/usecases/register-user-on-mailing-
 describe('Register user web controller', () => {
   const users: UserData[] = []
   const repo: UserRepository = new InMemoryUserRepository(users)
-  const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+  const useCase: UseCase = new RegisterUserOnMailingList(repo)
   const controller: RegisterUserController = new RegisterUserController(useCase)
+
+  class ErrorThrowingUseCaseStub implements UseCase {
+    perform (request: any): Promise<void> {
+      throw Error()
+    }
+  }
+
+  const errorThrowingUseCaseStub: UseCase = new ErrorThrowingUseCaseStub()
 
   test('should return status code 201 when request contains valid user data', async () => {
     const request: HttpRequest = {
@@ -79,5 +88,20 @@ describe('Register user web controller', () => {
     expect(response.statusCode).toEqual(400)
     expect(response.body).toBeInstanceOf(MissingParamError)
     expect((response.body as Error).message).toEqual('Missing parameter from request: name email.')
+  })
+
+  test('should return status code 500 when server raises', async () => {
+    const request: HttpRequest = {
+      body: {
+        name: 'Any name',
+        email: 'any@mail.com'
+      }
+    }
+
+    const controller: RegisterUserController = new RegisterUserController(errorThrowingUseCaseStub)
+    const response: HttpResponse = await controller.handle(request)
+
+    expect(response.statusCode).toEqual(500)
+    expect(response.body).toBeInstanceOf(Error)
   })
 })
