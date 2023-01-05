e0516390f43e80ffe21249bddcdeb59ece988b2c
wilson Neto <wngaspar10@gmail.com>
2023-01-05 10:48:07 -0300

    feat: apply cors enabling middleware

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,6 +1,8 @@
 import { bodyParser } from '@/main/config/middleware/body-parser'
+import { cors } from '@/main/config/middleware/cors'
 import { Express } from 'express'
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
