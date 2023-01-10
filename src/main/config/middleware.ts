1f45011dcde20729facd6bf64b37a1dd619f08b9
wilson Neto <wngaspar10@gmail.com>
2023-01-10 11:07:39 -0300

    refactor: organize imports

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,6 +1,4 @@
-import { bodyParser } from '@/main/config/middleware/body-parser'
-import { contentType } from '@/main/config/middleware/content-type'
-import { cors } from '@/main/config/middleware/cors'
+import { bodyParser, contentType, cors } from '@/main/config/middleware/'
 import { Express } from 'express'
 
 export default (app: Express): void => {
