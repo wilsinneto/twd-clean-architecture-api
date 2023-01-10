6e914a9c40a9cc71e97e9d119997e679b7ed29d3
wilson Neto <wngaspar10@gmail.com>
2023-01-10 16:14:07 -0300

    fix: order importing

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,6 @@
-import app from '@/main/config/app'
 import 'module-alias/register'
+
+import app from '@/main/config/app'
 
 app.listen(5000, () => {
   console.log('Server running at http://localhost:5000')
