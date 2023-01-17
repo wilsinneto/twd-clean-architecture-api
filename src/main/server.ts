448df6279d43f82add6f70a1109e96df526cd45b
wilson Neto <wngaspar10@gmail.com>
2023-01-17 16:29:23 -0300

    feat: connect to mongodb before listening

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,12 @@
 import 'module-alias/register'
 
-import app from '@/main/config/app'
+import { MongoHelper } from '@/external/repositories/mongodb/helper'
 
-app.listen(5000, () => {
-  console.log('Server running at http://localhost:5000')
-})
+MongoHelper.connect(process.env.MONGO_URL)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(process.env.PORT || 5000, () => {
+      console.log('Server running...')
+    })
+  })
+  .catch(console.error)
