4e78becab32249b831c0308a531d5ebf400a46b7
wilson Neto <wngaspar10@gmail.com>
2023-01-17 16:19:35 -0300

    test: find all users in a mongodb repository

--- test/external/repositories/mongodb/mongodb-user-repository.spec.ts
+++ test/external/repositories/mongodb/mongodb-user-repository.spec.ts
@@ -25,4 +25,22 @@ describe('Mongodb User repository', () => {
 
     expect(await userRepository.exists(user)).toBeTruthy()
   })
+
+  test('Find all users should return all added users', async () => {
+    const userRepository = new MongodbUserRepository()
+    await userRepository.add({
+      name: 'any_name',
+      email: 'any@mail.com'
+    })
+
+    await userRepository.add({
+      name: 'second_name',
+      email: 'second@mail.com'
+    })
+
+    const users = await userRepository.findAllUsers()
+
+    expect(users[0].name).toEqual('any_name')
+    expect(users[1].name).toEqual('second_name')
+  })
 })
