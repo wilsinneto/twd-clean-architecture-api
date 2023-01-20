a704276f170e0210b958da18d93f1493ab46ee09
wilson Neto <wngaspar10@gmail.com>
2023-01-20 14:37:48 -0300

    fix: use mongodb correct url

--- src/main/server.ts
+++ src/main/server.ts
@@ -2,11 +2,11 @@ import 'module-alias/register'
 
 import { MongoHelper } from '@/external/repositories/mongodb/helper'
 
-MongoHelper.connect(process.env.MONGO_URL)
+MongoHelper.connect('mongodb://localhost')
   .then(async () => {
     const app = (await import('./config/app')).default
     app.listen(process.env.PORT || 5000, () => {
-      console.log('Server running...')
+      console.log('Server running at http://localhost:5000')
     })
   })
   .catch(console.error)
