11415ea79575ad1745804c8e85a2b266a45377d3
wilson Neto <wngaspar10@gmail.com>
2023-02-15 10:01:56 -0300

    feat: handle invalid user data

--- src/usecases/send-email/send-email.ts
+++ src/usecases/send-email/send-email.ts
@@ -1,5 +1,6 @@
-import { UserData } from '@/entities'
-import { Either } from '@/shared'
+import { User, UserData } from '@/entities'
+import { InvalidEmailError, InvalidNameError } from '@/entities/errors'
+import { Either, left } from '@/shared'
 import { MailServiceError } from '@/usecases/errors'
 import { UseCase } from '@/usecases/ports'
 import { EmailOptions, EmailService } from '@/usecases/send-email/ports'
@@ -13,9 +14,17 @@ export class SendEmail implements UseCase {
     this.emailService = emailService
   }
 
-  perform (userData: UserData): Promise<Either<MailServiceError, EmailOptions>> {
-    // const greetings = 'E aí <b>' + userData.name + '</b>, beleza?'
-    // const customizedHtml = greetings + '<br> <br>' + this.emailOptions.html
+  async perform (userData: UserData): Promise<Either<InvalidNameError | InvalidEmailError | MailServiceError, EmailOptions>> {
+    const userOrError: Either<InvalidNameError | InvalidEmailError, User> = User.create(userData)
+
+    if (userOrError.isLeft()) {
+      return left(userOrError.value)
+    }
+
+    const user = userOrError.value
+
+    const greetings = 'E aí <b>' + user.name + '</b>, beleza?'
+    const customizedHtml = greetings + '<br> <br>' + this.emailOptions.html
 
     const emailInfo: EmailOptions = {
       host: this.emailOptions.host,
@@ -23,13 +32,13 @@ export class SendEmail implements UseCase {
       username: this.emailOptions.username,
       password: this.emailOptions.password,
       from: this.emailOptions.from,
-      to: userData.name + '<' + userData.email + '>',
+      to: user.name + '<' + user.email + '>',
       subject: this.emailOptions.subject,
       text: this.emailOptions.text,
-      html: this.emailOptions.html,
+      html: customizedHtml,
       attachments: this.emailOptions.attachments
     }
 
-    return this.emailService.send(emailInfo)
+    return await this.emailService.send(emailInfo)
   }
 }
