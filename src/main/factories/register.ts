a84b120099814efa790a5bd0ff864f54115eb2ce
wilson Neto <wngaspar10@gmail.com>
2023-02-23 10:17:01 -0300

    feat: use register and send email use case

--- src/main/factories/register.ts
+++ src/main/factories/register.ts
@@ -1,11 +1,18 @@
+import { NodemailerEmailService } from '@/external/mail-services'
 import { MongodbUserRepository } from '@/external/repositories/mongodb'
+import { getEmailOptions } from '@/main/config/email'
+import { RegisterAndSendEmail } from '@/usecases/register-and-send-email'
 import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
+import { SendEmail } from '@/usecases/send-email'
 import { RegisterAndSendEmailController } from '@/web-controllers/'
 
-export const makeRegisterUserController = (): RegisterAndSendEmailController => {
+export const makeRegisterAndSendEmailController = (): RegisterAndSendEmailController => {
   const mongoDbUserRepository = new MongodbUserRepository()
   const registerUserOnMailingListUseCase = new RegisterUserOnMailingList(mongoDbUserRepository)
-  const registerUserController = new RegisterAndSendEmailController(registerUserOnMailingListUseCase)
+  const emailService = new NodemailerEmailService()
+  const sendEmailUseCase = new SendEmail(getEmailOptions(), emailService)
+  const registerAndSendEmailUseCase = new RegisterAndSendEmail(registerUserOnMailingListUseCase, sendEmailUseCase)
+  const registerAndSendEmailController = new RegisterAndSendEmailController(registerAndSendEmailUseCase)
 
-  return registerUserController
+  return registerAndSendEmailController
 }
--- src/main/routes/register-route.ts
+++ src/main/routes/register-route.ts
@@ -1,7 +1,7 @@
 import { adaptRoute } from '@/main/adapters'
-import { makeRegisterUserController } from '@/main/factories'
+import { makeRegisterAndSendEmailController } from '@/main/factories'
 import { Router } from 'express'
 
 export default (router: Router): void => {
-  router.post('/register', adaptRoute(makeRegisterUserController()))
+  router.post('/register', adaptRoute(makeRegisterAndSendEmailController()))
 }
