adf7eb04a5646fca21a182a360cfde973366bd05
wilson Neto <wngaspar10@gmail.com>
2023-02-23 16:03:18 -0300

    refactor: add mongo url

--- src/main/server.ts
+++ src/main/server.ts
@@ -2,7 +2,7 @@ import 'module-alias/register'
 
 import { MongoHelper } from '@/external/repositories/mongodb/helper'
 
-MongoHelper.connect('mongodb://localhost')
+MongoHelper.connect(process.env.MONGO_URL)
   .then(async () => {
     const app = (await import('./config/app')).default
     app.listen(process.env.PORT || 5000, () => {
